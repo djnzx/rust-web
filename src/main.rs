d19249162635a858dd0a70358270a0def84b7817
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2024-10-11 12:57:43 +0300

    - day 22

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
+use std::{fs, thread};
+use std::io::BufReader;
+use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+use std::time::Duration;
+use rust_web::ThreadPool;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming()
+        // .take(2)
+    {
+        let stream = stream.unwrap();
+        println!("Connection established!");
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+
+        // thread per request ...
+        // thread::spawn(|| {
+        //     handle_connection(stream);
+        // });
+    }
+    println!("Shutting down.");
 }
+
+fn handle_connection0(mut stream: TcpStream) {
+    let br = BufReader::new(&mut stream);
+    let http_request = br
+        .lines()
+        .map(|x| x.unwrap())
+        // read http header till empty line
+        .take_while(|x| !x.is_empty())
+        .collect::<Vec<_>>();
+
+    println!("Request: {http_request:#?}");
+    // we can't respond till read all the content
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn handle_connection1(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
